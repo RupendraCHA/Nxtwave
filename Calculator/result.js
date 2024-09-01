238a7538896b914e11100d4a23765f7b6dec2721
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-09-02 05:08:36 +0530

    Node js initial project

---
+++ Calculator/result.js
@@ -0,0 +1,3 @@
+const sum = require("./calculator.js")
+
+console.log(sum(4, 9))
---
+++ index.js
@@ -0,0 +1,8 @@
+const greeting = name => {
+    console.log(`hello ${name} Chandaluri`)
+}
+
+greeting("Chiranjeevi");
+greeting("Vasundhara");
+greeting("Gopi");
+greeting("Rupendra");
